445e604ab7ab1d3f3b3192b5db396af597dc9529
juliano <julianojd@ymail.com>
2020-03-12 22:34:46 -0300

    Initial Commit

---
+++ viewcodetemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viewcodetemplate
+//
+//  Created by juliano jose dziadzio on 12/03/20.
+//  Copyright © 2020 juliano jose dziadzio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ viewcodetemplateTests/viewcodetemplateTests.swift
@@ -0,0 +1,34 @@
+//
+//  viewcodetemplateTests.swift
+//  viewcodetemplateTests
+//
+//  Created by juliano jose dziadzio on 12/03/20.
+//  Copyright © 2020 juliano jose dziadzio. All rights reserved.
+//
+
+import XCTest
+@testable import viewcodetemplate
+
+class viewcodetemplateTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
